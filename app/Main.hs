d348ab8444d9b108c8315600debf69ad969c4610
Coding-Cactus <codingcactus.cc@gmail.com>
2024-03-12 23:20:54 +0000

    ✨ add cli interface

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,31 @@
--- This is the main entry point for your Hurtle viewer.
+module Main (main) where
+
+import System.Directory (doesFileExist)
+import System.Environment (getArgs)
+
+import Hurtle.Types
+import Hurtle.Parser
+import Hurtle.Viewer
 
 main :: IO ()
-main = error "Not implemented!\n(find me in app/Main.hs)\n"
+main = do
+  args <- getArgs
+  case args of
+    [fname] -> runFile fname
+    _       -> putStrLn "Unexpected number of arguments, expected 1 argument:\n  - the file name of the hogo program"
+
+
+runFile :: String -> IO ()
+runFile fname = do
+  fileExists <- doesFileExist fname
+  if fileExists
+    then do
+      content <- readFile fname
+      case parseHogoFile fname content of
+        Left err -> putStrLn err
+        Right program -> runHogo program
+    else putStrLn $ "File " <> fname <> " not found"
+
+
+runHogo :: HogoProgram -> IO ()
+runHogo program = renderHogo program
