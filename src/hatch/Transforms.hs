ff59f2af0cd7df3955748c499f0a38418451a494
Coding-Cactus <codingcactus.cc@gmail.com>
2024-03-19 18:29:10 +0000

    🎨 follow style of other hatch functions

--- src/hatch/Transforms.hs
+++ src/hatch/Transforms.hs
@@ -10,7 +10,6 @@ module Transforms where
 import Layout
 import qualified Graphics.Gloss as Gloss
 
-import Graphics.Gloss (color)
 import Graphics.Gloss.Data.Color
 
 --------------------------------------------------------------------------------
@@ -30,7 +29,7 @@ mirror :: Image -> Image
 mirror = fmap $ Gloss.scale (-1) 1
 
 applyColour :: Int -> Int -> Int -> Image -> Image
-applyColour r g b = fmap (color rgbColor)
-  where rgbColor = makeColorI r g b 255
+applyColour r g b = fmap (Gloss.color rgbColour)
+  where rgbColour = makeColorI r g b 255
 
 --------------------------------------------------------------------------------
